7a889ac27e58281e380ab3f4c21a7033ebc10598
jpm <jpmckenna90@gmail.com>
2020-01-25 16:30:30 -0500

    Wrote some inquirer prompts, installed axios

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is your name?",
-    name: "name"
-  }]).then(function(response) {
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your github username?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What's your favorite color?",
+      name: "color"
+    }
+  ])
+  .then(function(response) {
     console.log(response);
-  })
-
+  });
