dd0c3f9be6c6cbd4d9236de880079893fc39e9a9
jpm <jpmckenna90@gmail.com>
2020-01-25 17:03:28 -0500

    Built constructor, successfully passing all data from axios call into constructor and creating new object from that data

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
+// constructor
+function User(name, username, imgURL, bio, location, repos, following, followers){ //// ! DON'T FORGET TO DO STARS - doesn't appear to be in res.data 
+  this.name = name,
+  this.username = username,
+  this.imgURL = imgURL,
+  this.bio = bio,
+  this.location = location,
+  this.repos = repos,
+  this.following = following,
+  this.followers = followers
+  // this.stars = stars
+}
+
+
 inquirer
   .prompt([
     {
@@ -23,7 +37,22 @@ inquirer
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}`
     axios.get(queryUrl).then(function(res){
-      console.log(res.data)
+
+      // Gather all data from axios response to pass into constructor
+      const name = res.data.name;
+      const username = res.data.login;
+      const imgURL = res.data.avatar_url;
+      const bio = res.data.bio;
+      const location = res.data.location;
+      const repos = res.data.public_repos;
+      const followers = res.data.followers;
+      const following = res.data.following;
+      // ! DON'T FORGET TO DO STARS - doesn't appear to be in res.data 
+
+      // Create new user with constructor 
+      const user = new User(name, username, imgURL, bio, location, repos, followers, following);
+      console.log(user);
+
     })
   });
 // .then(function(response) {
