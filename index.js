37c502f74d64400513db6973cd14af1cc28d72c5
jpm <jpmckenna90@gmail.com>
2020-01-25 16:25:26 -0500

    Installed inquirer

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name"
+  }]).then(function(response) {
+    console.log(response);
+  })
+
