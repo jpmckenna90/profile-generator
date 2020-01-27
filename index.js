542456a56ee7c8f4c051fdd60200c6129a995bdf
jpm <jpmckenna90@gmail.com>
2020-01-26 19:53:17 -0500

    Added some styling tweaks

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
-
 // import writeHtml from "./writehtml.js";
 
 // constructor
@@ -29,84 +28,89 @@ function User(
 
 const writeHtml = user => {
   return `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
-    <title>Profile Generator</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 text-center">
-          <h1 class="mx-auto">
-            ${user.name}
-          </h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="mx-auto">
-            Github Username: ${user.username}
-          </h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="mx-auto">
-            <img src="${user.imgURL}">
-          </h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12 text-center">
-            <h3 class="mx-auto">
-              ${user.bio}
-            </h3>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-6 text-center">
-          <div class="card">
-            <h3 class="mx-auto">
-              Public Repos: ${user.repos}
-            </h3>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <title>Profile Generator</title>
+      <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="top">
+          <div class="container">
+            <div class="jumbotron" id="jumbotronid">
+              <div class="row">
+                <div class="col-12 text-center">
+                  <h1 class="mx-auto">
+                    ${user.name}
+                  </h1>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 text-center">
+                  <h2 class="mx-auto">
+                    Github Username: ${user.username}
+                  </h2>
+                </div>
+              </div>
+              <div class="row">
+              <div class="col-12 text-center">
+                  <h3 class="mx-auto">
+                    ${user.bio}
+                  </h3>
+              </div>
+            </div>
+              <div class="row" id="imgrow">
+                <div class="col-12 text-center">
+                  <h2 class="mx-auto">
+                    <img src="${user.imgURL}" alt="Profile Picture" id="biopic">
+                  </h2>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="col-6 text-center">
-          <div class="card">
-            <h3 class="mx-auto">
-              Starred Repos: ${user.stared}
-            </h3>
+        <div class="row">
+          <div class="col-6 text-center">
+            <div class="card">
+              <h3 class="mx-auto">
+                Public Repos: ${user.repos}
+              </h3>
+            </div>
+          </div>
+          <div class="col-6 text-center">
+            <div class="card">
+              <h3 class="mx-auto">
+                Starred Repos Here!
+              </h3>
+            </div>
           </div>
         </div>
+        <div class="row">
+          <div class="col-6 text-center">
+            <div class="card">
+              <h3 class="mx-auto">
+                Followers: ${user.followers}
+              </h3>
+            </div>
+          </div>
+          <div class="col-6 text-center">
+            <div class="card">
+              <h3 class="mx-auto">
+                Following: ${user.following}
+              </h3>
+            </div>
       </div>
-      <div class="row">
-        <div class="col-6 text-center">
-          <div class="card">
-            <h3 class="mx-auto">
-              Followers: ${user.followers}
-            </h3>
-          </div>
-        </div>
-        <div class="col-6 text-center">
-          <div class="card">
-            <h3 class="mx-auto">
-              Following: ${user.following}
-            </h3>
-          </div>
-    </div>
-  </body>
-</html>`;
+    </body>
+  </html>`;
 };
-
 
 inquirer
   .prompt([
@@ -129,7 +133,7 @@ inquirer
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}`;
     const starredUrl = `https://api.github.com/users/${username}/starred`;
-    
+
     axios.get(queryUrl).then(function(res) {
       // Gather all data from axios response to pass into constructor
       const name = res.data.name;
@@ -140,8 +144,6 @@ inquirer
       const repos = res.data.public_repos;
       const followers = res.data.followers;
       const following = res.data.following;
-    
-
 
       // ! DON'T FORGET TO DO STARS - doesn't appear to be in res.data
 
